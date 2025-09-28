d88dbee8a84897dd4cf6fb3d5eb981ea1f26e3fd
Митракова Екатерина <katka.ice@gmail.com>
2025-09-28 20:21:24 +0300

    'commit'

--- README.md
+++ README.md
@@ -71,8 +71,17 @@ def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     if nums == []:
         return ValueError('ValueError') 
     '''Возвращает ValueError, если список пустой'''
+    
+    mini = nums[0]
+    for next in nums:
+        if next < mini:
+            mini = next 
+    maxi = nums[0]
+    for next in nums:
+        if next > mini:
+            maxi = next 
 
-    return (min(nums), max(nums))  
+    return mini, maxi 
     '''В другом случае возвращает минимум и максимум из списка'''
 
 print('[3, -1, 5, 5, 0] ->', min_max([3, -1, 5, 5, 0]))
@@ -83,3 +92,54 @@ print('[1.5, 2, 2.0, -3.1] ->', min_max([1.5, 2, 2.0, -3.1]))
 ```
 
 ![Задание 1(min_max)](/images/lab02/01_arrays_min_max.png)
+
+### Задание 1 (unique_sorted)
+
+```
+def unique_sorted(nums: list[float | int]) -> list[float | int]:
+
+    nums = list(set(nums))
+    n = len(nums)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+                '''Сортировка пузырьком'''
+                
+    return nums
+
+print('[3, 1, 2, 1, 3] ->', unique_sorted([3, 1, 2, 1, 3]))
+print('[] ->', unique_sorted([]))
+print('[-1, -1, 0, 2, 2] ->', unique_sorted([-1, -1, 0, 2, 2]))
+print('[1.0, 1, 2.5, 2.5, 0] ->', unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+```
+
+![Задание 1(unique_sorted)](/images/lab02/01_arrays_unique_sorted.png)
+
+### Задание 1 (flatten)
+
+```
+def flatten(mat: list[list | tuple]) -> list:
+    
+    new_mat = []
+    for elements in mat:
+            if isinstance(elements, tuple | list):
+                '''Проверяет, список.кортеж ли элемент'''
+
+                for element in elements:
+                    new_mat.append(element)
+                '''Если все элементы удовлетворяют условию, то проходимся по элементам внутри каждого из них'''
+                
+            else:
+                return TypeError('TypeError')
+            '''Если есть элемент, не являющийся списком/кортежем, выводит ошибку'''
+
+    return new_mat
+
+print('[[1, 2], [3, 4]] ->', flatten([[1, 2], [3, 4]]))
+print('([1, 2], (3, 4, 5)) ->', flatten(([1, 2], (3, 4, 5))))
+print('[[1], [], [2, 3]] ->', flatten([[1], [], [2, 3]]))
+print('[[1, 2], "ab"] ->', flatten([[1, 2], "ab"]))
+```
+![Задание 1(flatten)](/images/lab02/01_arrays_flatten.png)
--- src/lab02/01_arrays.py
+++ src/lab02/01_arrays.py
@@ -1,14 +1,64 @@
-def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
+# def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
 
-    if nums == []:
-        return ValueError('ValueError') 
-    '''Возвращает ValueError, если список пустой'''
+#     if nums == []:
+#         return ValueError('ValueError') 
+#     '''Возвращает ValueError, если список пустой'''
 
-    return (min(nums), max(nums))  
-    '''В другом случае возвращает минимум и максимум из списка'''
+#     mini = nums[0]
+#     for next in nums:
+#         if next < mini:
+#             mini = next 
+#     maxi = nums[0]
+#     for next in nums:
+#         if next > mini:
+#             maxi = next 
 
-print('[3, -1, 5, 5, 0] ->', min_max([3, -1, 5, 5, 0]))
-print('[42] ->', min_max([42]))
-print('[-5, -2, -9] ->', min_max([-5, -2, -9]))
-print('[] ->', min_max([]))
-print('[1.5, 2, 2.0, -3.1] ->', min_max([1.5, 2, 2.0, -3.1]))
+#     return mini, maxi 
+#     '''В другом случае возвращает минимум и максимум из списка'''
+
+# print('[3, -1, 5, 5, 0] ->', min_max([3, -1, 5, 5, 0]))
+# print('[42] ->', min_max([42]))
+# print('[-5, -2, -9] ->', min_max([-5, -2, -9]))
+# print('[] ->', min_max([]))
+# print('[1.5, 2, 2.0, -3.1] ->', min_max([1.5, 2, 2.0, -3.1]))
+
+# def unique_sorted(nums: list[float | int]) -> list[float | int]:
+
+#     nums = list(set(nums))
+#     n = len(nums)
+#     for i in range(n):
+#         for j in range(0, n - i - 1):
+#             if nums[j] > nums[j + 1]:
+#                 nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
+#                 '''Сортировка пузырьком'''
+
+#     return nums
+
+# print('[3, 1, 2, 1, 3] ->', unique_sorted([3, 1, 2, 1, 3]))
+# print('[] ->', unique_sorted([]))
+# print('[-1, -1, 0, 2, 2] ->', unique_sorted([-1, -1, 0, 2, 2]))
+# print('[1.0, 1, 2.5, 2.5, 0] ->', unique_sorted([1.0, 1, 2.5, 2.5, 0]))
+
+
+def flatten(mat: list[list | tuple]) -> list:
+    
+    new_mat = []
+    for elements in mat:
+            if isinstance(elements, tuple | list):
+                '''Проверяет, список.кортеж ли элемент'''
+
+                for element in elements:
+                    new_mat.append(element)
+                '''Если все элементы удовлетворяют условию, то проходимся по элементам внутри каждого из них'''
+                
+            else:
+                return TypeError('TypeError')
+            '''Если есть элемент, не являющийся списком/кортежем, выводит ошибку'''
+
+    return new_mat
+
+print('[[1, 2], [3, 4]] ->', flatten([[1, 2], [3, 4]]))
+print('([1, 2], (3, 4, 5)) ->', flatten(([1, 2], (3, 4, 5))))
+print('[[1], [], [2, 3]] ->', flatten([[1], [], [2, 3]]))
+print('[[1, 2], "ab"] ->', flatten([[1, 2], "ab"]))
