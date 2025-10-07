4440c44058b6ddf0a650465ffc77bb07fa71e120
Митракова Екатерина <katka.ice@gmail.com>
2025-10-08 00:45:29 +0300

    lib add

--- src/lab02/02_matrix.py
+++ src/lab02/02_matrix.py
@@ -1,55 +1,55 @@
-# def transpose(mat: list[list[float | int]]) -> list[list]:
+def transpose(mat: list[list[float | int]]) -> list[list]:
 
-#     if not mat:
-#         return []
-#         '''Проверка наличия элементов в матрице'''
-#     len_row = len(mat[0])
-#     len_col = len(mat)
+    if not mat:
+        return []
+        '''Проверка наличия элементов в матрице'''
+    len_row = len(mat[0])
+    len_col = len(mat)
     
-#     for num in range(len(mat) - 1):
-#         if len(mat[num]) != len(mat[num + 1]):
-#             return ValueError('ValueError')
-#             '''Проверка на одинаковую длину строк'''
+    for num in range(len(mat) - 1):
+        if len(mat[num]) != len(mat[num + 1]):
+            return ValueError('ValueError')
+            '''Проверка на одинаковую длину строк'''
 
-#     new_mat =[]
-#     for col_ind in range(len_row):
-#         new_row = []
-#         '''С каждым запуске цикла создаётся ряд, рядов столько, сколько столбцов в изначальной матрице'''
-#         for row_ind in range(len_col):
-#             new_row.append(mat[row_ind][col_ind])
-#             '''Элементов в ряд добавляется столько, сколько столбцов в изначальной матрице'''
-#         new_mat.append(new_row)
-#         '''Ряд добавляется в новую матрицу'''
+    new_mat =[]
+    for col_ind in range(len_row):
+        new_row = []
+        '''С каждым запуске цикла создаётся ряд, рядов столько, сколько столбцов в изначальной матрице'''
+        for row_ind in range(len_col):
+            new_row.append(mat[row_ind][col_ind])
+            '''Элементов в ряд добавляется столько, сколько столбцов в изначальной матрице'''
+        new_mat.append(new_row)
+        '''Ряд добавляется в новую матрицу'''
 
-#     return new_mat
+    return new_mat
 
 
-# print('[[1, 2, 3]] ->', transpose([[1, 2, 3]]))
-# print('[[1], [2], [3]] ->', transpose([[1], [2], [3]]))
-# print('[[1, 2], [3]] ->', transpose([[1, 2], [3, 4]]))
-# print('[] ->', transpose([]))
-# print('[[1, 2], [3]] ->', transpose([[1, 2], [3]]))
+print('[[1, 2, 3]] ->', transpose([[1, 2, 3]]))
+print('[[1], [2], [3]] ->', transpose([[1], [2], [3]]))
+print('[[1, 2], [3]] ->', transpose([[1, 2], [3, 4]]))
+print('[] ->', transpose([]))
+print('[[1, 2], [3]] ->', transpose([[1, 2], [3]]))
 
-# def row_sums(mat: list[list[float | int]]) -> list[float]:
+def row_sums(mat: list[list[float | int]]) -> list[float]:
 
-#     len_row = len(mat[0])
-#     len_col = len(mat)
+    len_row = len(mat[0])
+    len_col = len(mat)
     
-#     for num in range(len(mat) - 1):
-#         if len(mat[num]) != len(mat[num + 1]):
-#             return ValueError('ValueError')
-#             '''Проверка на одинаковую длину строк'''
+    for num in range(len(mat) - 1):
+        if len(mat[num]) != len(mat[num + 1]):
+            return ValueError('ValueError')
+            '''Проверка на одинаковую длину строк'''
 
-#     sum_row = []
-#     for row in mat:
-#         sum_row.append(sum(row))
+    sum_row = []
+    for row in mat:
+        sum_row.append(sum(row))
 
-#     return sum_row
+    return sum_row
 
-# print('[[1, 2, 3], [4, 5, 6]] ->', row_sums([[1, 2, 3], [4, 5, 6]]))
-# print('[[-1, 1], [10, -10]] ->', row_sums([[-1, 1], [10, -10]]))
-# print('[[0, 0], [0, 0]] ->', row_sums([[0, 0], [0, 0]]))
-# print('[[1, 2], [3]] ->', row_sums([[1, 2], [3]]))
+print('[[1, 2, 3], [4, 5, 6]] ->', row_sums([[1, 2, 3], [4, 5, 6]]))
+print('[[-1, 1], [10, -10]] ->', row_sums([[-1, 1], [10, -10]]))
+print('[[0, 0], [0, 0]] ->', row_sums([[0, 0], [0, 0]]))
+print('[[1, 2], [3]] ->', row_sums([[1, 2], [3]]))
 
 def col_sums(mat: list[list[float | int]]) -> list[float]:
 
@@ -69,6 +69,7 @@ def col_sums(mat: list[list[float | int]]) -> list[float]:
         sum_col.append(summa)
     
     return sum_col
+
 print('[[1, 2, 3], [4, 5, 6]] ->', col_sums([[1, 2, 3], [4, 5, 6]]))
 print('[[-1, 1], [10, -10]] ->', col_sums([[-1, 1], [10, -10]]))
 print('[[0, 0], [0, 0]] ->', col_sums([[0, 0], [0, 0]]))
---
+++ src/lib/matrix.py
@@ -0,0 +1,54 @@
+def transpose(mat: list[list[float | int]]) -> list[list]:   #Меняет строки и столбцы
+
+    if not mat:
+        return []
+
+    len_row = len(mat[0])
+    len_col = len(mat)
+    
+    for num in range(len(mat) - 1):
+        if len(mat[num]) != len(mat[num + 1]):
+            return ValueError('ValueError')
+
+
+    new_mat =[]
+    for col_ind in range(len_row):
+        new_row = []
+        for row_ind in range(len_col):
+            new_row.append(mat[row_ind][col_ind])
+        new_mat.append(new_row)
+
+    return new_mat
+
+def row_sums(mat: list[list[float | int]]) -> list[float]:    #Считает сумму по ряду
+
+    len_row = len(mat[0])
+    len_col = len(mat)
+    
+    for num in range(len(mat) - 1):
+        if len(mat[num]) != len(mat[num + 1]):
+            return ValueError('ValueError')
+
+    sum_row = []
+    for row in mat:
+        sum_row.append(sum(row))
+
+    return sum_row
+
+def col_sums(mat: list[list[float | int]]) -> list[float]:    #Считает сумму по столбцу
+
+    len_row = len(mat[0])
+    len_col = len(mat)
+    
+    for num in range(len(mat) - 1):
+        if len(mat[num]) != len(mat[num + 1]):
+            return ValueError('ValueError')
+
+    sum_col = []
+    for col in range(len_row):
+        summa = 0
+        for row in range(len_col):
+            summa += mat[row][col]
+        sum_col.append(summa)
+    
+    return sum_col
